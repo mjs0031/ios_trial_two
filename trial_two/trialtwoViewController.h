4fa68ddc6c7b6cbb7e49e78a117dd0f901620b76
Matthew J Swann <mjs0031@auburn.edu>
2014-06-02 19:15:07 -0500

    additional components

--- trial_two/trialtwoViewController.h
+++ trial_two/trialtwoViewController.h
@@ -8,15 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface trialtwoViewController : UIViewController
+@interface trialtwoViewController : UIViewController<UIActionSheetDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *name_field;
 
 @property (weak, nonatomic) IBOutlet UITextField *number_field;
 
+@property (weak, nonatomic) IBOutlet UILabel *slider_value_label;
+
+@property (weak, nonatomic) IBOutlet UISwitch *left_switch;
+
+@property (weak, nonatomic) IBOutlet UISwitch *right_switch;
+
+@property (weak, nonatomic) IBOutlet UIButton *do_something_button;
+
 - (IBAction)textFieldDoneEditing:(id)sender;
 
 - (IBAction)backgroundTap:(id)sender;
 
+- (IBAction)slider_changed:(UISlider *)sender;
+
+- (IBAction)switch_switched:(UISwitch *)sender;
+
+- (IBAction)toggle_controls:(UISegmentedControl *)sender;
+
+- (IBAction)button_pressed:(UIButton *)sender;
 
 @end
--- trial_two/trialtwoViewController.m
+++ trial_two/trialtwoViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.slider_value_label.text = @"50";
 }
 
 - (void)didReceiveMemoryWarning
@@ -33,6 +34,70 @@
 - (IBAction)backgroundTap:(id)sender {
     [self.name_field   resignFirstResponder];
     [self.number_field resignFirstResponder];
+}
+
+- (IBAction)slider_changed:(UISlider *)sender {
+    int output = lroundf(sender.value);
+    self.slider_value_label.text = [NSString stringWithFormat:@"%d", output];
+}
+
+- (IBAction)switch_switched:(UISwitch *)sender {
+    BOOL toggle = sender.isOn;
+    [self.left_switch setOn:toggle animated:YES];
+    [self.right_switch setOn:toggle animated:YES];
+}
+
+- (IBAction)toggle_controls:(UISegmentedControl *)sender {
+    // 0 == switches index
+    if (sender.selectedSegmentIndex == 0) {
+        self.left_switch.hidden  = NO;
+        self.right_switch.hidden = NO;
+        self.do_something_button.hidden = YES;
+    } else {
+        self.left_switch.hidden  = YES;
+        self.right_switch.hidden = YES;
+        self.do_something_button.hidden = NO;
+    }
+}
+
+- (IBAction)button_pressed:(UIButton *)sender {
+    
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+        initWithTitle         :@"are you sure?"
+        delegate              :self
+        cancelButtonTitle     :@"no way!"
+        destructiveButtonTitle:@"yes, proceed."
+        otherButtonTitles     :nil];
+    
+    [actionSheet showInView:self.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex != [actionSheet cancelButtonIndex])
+    {
+        NSString *msg = nil;
+        if (self.name_field.text.length > 0)
+        
+        {
+            msg = [NSString stringWithFormat:
+                   @"You can breathe easy, %@, everything went OK.",
+                   self.name_field.text];
+        }
+        else
+        {
+            msg = @"You can breathe easy, everything went OK.";
+        }
+        
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle     :@"Something was done"
+                              message           :msg
+                              delegate          :self
+                              cancelButtonTitle :@"Phew!"
+                              otherButtonTitles :nil];
+        [alert show];
+        
+    }
 }
 
 @end
